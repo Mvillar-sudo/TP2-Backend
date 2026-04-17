fd0c2470ecdf0e593b8a5ec192da8b5031a1d319
Manu Villar <manuvillar@fi.uba.ar>
2026-04-17 14:23:00 -0300

    Realizo cambios al archivo dump.sql siendo estos la eliminacion de la Ciudad y el Estadio de toda la tabla.

--- dump.sql
+++ dump.sql
@@ -7,16 +7,14 @@ CREATE TABLE partidos (
     Equipo_visitante VARCHAR(100) NOT NULL,
     Goles_local      INT NULL,
     Goles_visitante  INT NULL,
-    Estadio          VARCHAR(100),
-    Ciudad           VARCHAR(100),
-    Fecha            DATETIME NOT NULL,
+    Fecha            DATE NOT NULL,
     Fase             VARCHAR(50) NOT NULL
 );
 
-INSERT INTO partidos (Equipo_local, Equipo_visitante, Estadio, Ciudad, Fecha, Fase)
+INSERT INTO partidos (Equipo_local, Equipo_visitante, Fecha, Fase)
 VALUES
-('Argentina', 'Croacia', 'Estadio Azteca', 'Ciudad de Mexico', '2026-03-11 15:00:00', 'Grupos'),
-('Brasil', 'Paises Bajos', 'Estadio Azteca', 'Ciudad de Mexico', '2026-03-11 18:00:00', 'Grupos'),
-('Senegal', 'España', 'Estadio Azteca', 'Ciudad de Mexico', '2026-03-13 15:00:00', 'Grupos'),
-('Costa Rica', 'Uruguay', 'Estadio Azteca', 'Ciudad de Mexico', '2026-03-13 18:00:00', 'Grupos');
+('Argentina', 'Croacia', '2026-03-11', 'Grupos'),
+('Brasil', 'Paises Bajos', '2026-03-11', 'Grupos'),
+('Senegal', 'España', '2026-03-13', 'Grupos'),
+('Costa Rica', 'Uruguay', '2026-03-13', 'Grupos');
 
