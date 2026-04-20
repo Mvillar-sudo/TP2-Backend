7615a72a2247241dd897578013282a91b72970a8
Manu Villar <manuvillar@fi.uba.ar>
2026-04-20 14:59:24 -0300

    Cambio el schema.sql para poder modificar sus errores presentes en la anterior version.

--- schema.sql
+++ schema.sql
@@ -1,20 +1,25 @@
-CREATE DATABASE IF NOT EXISTS mundial_db;
-USE mundial_db;
+DROP TABLE partidos;
+DROP TABLE usuarios;
+DROP TABLE predicciones;
+
+
+CREATE DATABASE IF NOT EXISTS mundial_fixture;
+USE mundial_fixture;
+
+CREATE TABLE partidos (
+    ID               INT AUTO_INCREMENT PRIMARY KEY,
+    Equipo_local     VARCHAR(100) NOT NULL,
+    Equipo_visitante VARCHAR(100) NOT NULL,
+    Goles_local      INT NULL,
+    Goles_visitante  INT NULL,
+    Fecha            DATE NOT NULL,
+    Fase             VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS usuarios (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nombre VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS partidos (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Equipo_local VARCHAR(255) NOT NULL,
-    Equipo_visitante VARCHAR(255) NOT NULL,
-    Goles_local INT NULL,
-    Goles_visitante INT NULL,
-    Fecha DATE NOT NULL,
-    Fase VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS predicciones (
